d242615ab8fa01febb748b55023cc3c1feb9326b
Morgan <morgancrosta@gmail.com>
2018-07-10 14:02:39 -0700

    deops

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+
+
 app.use(session({
   secret: 'work hard',
   resave: true,
@@ -27,6 +29,9 @@ app.use(session({
 // Add routes, both API and view
 app.use('/api', APIroutes);
 
+app.use('*', function(req,res){
+  res.sendFile("index.html", {root: __dirname + "/client/build"})
+})
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bagsdb");
 
